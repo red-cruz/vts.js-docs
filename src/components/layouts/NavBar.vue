d66e96bdfa78f8136b735922edd38f850ff2d5aa
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-10-04 22:23:35 +0800

    update navigation

--- src/components/layouts/NavBar.vue
+++ src/components/layouts/NavBar.vue
@@ -22,45 +22,72 @@
         <ul class="nav nav-underline flex-column" style="--bs-nav-underline-gap: 1px">
           <li class="nav-item">
             <form class="d-flex" role="search">
-              <div class="input-group">
-                <input
-                  type="search"
-                  name="search"
-                  class="form-control rounded-0"
-                  placeholder="Search"
-                  aria-label="Search"
-                  aria-desribedby="search"
-                  required
-                />
-                <button class="btn btn-outline-secondary rounded-0" type="submit" id="search">
-                  <Icon icon="bi:search" />
+              <!-- <div class="input-group"> -->
+              <input
+                type="search"
+                name="search"
+                class="form-control rounded-0"
+                placeholder="Search"
+                aria-label="Search"
+                aria-desribedby="search"
+                required
+              />
+              <!-- <button class="btn btn-outline-secondary rounded-0" type="submit" id="search">
+                  <Icon icon="mdi:magnify" height="25" />
                 </button>
-              </div>
+              </div> -->
             </form>
           </li>
-          <NavItem href="/">Introduction</NavItem>
-          <li class="nav-item m-0">
+          <NavItem href="/">
+            <Icon icon="mdi:book-open-variant" />
+            Introduction
+          </NavItem>
+          <li class="nav-item">
             <NavItem href="/installation">
-              <i class="fa-solid fa-download"></i> Installation
+              <Icon icon="mdi:download" />
+              Installation
             </NavItem>
             <ul class="navbar-nav ps-4" id="installation">
-              <NavItem :href="{ name: 'installation', hash: '#CDN' }" active-class="">CDN</NavItem>
-              <NavItem :href="{ name: 'installation', hash: '#NPM' }" active-class="">NPM</NavItem>
+              <SubItem :href="{ name: 'installation', hash: '#CDN' }">CDN</SubItem>
+              <SubItem :href="{ name: 'installation', hash: '#NPM' }">NPM</SubItem>
             </ul>
           </li>
           <li class="nav-item">
             <NavItem href="/configuration">
-              <i class="fa-solid fa-sliders"></i> Configuration
+              <Icon icon="mdi:cog-outline" />
+              Configuration
             </NavItem>
-            <ul class="navbar-nav ps-4">
-              <NavItem href="/configuration#Ajax">Ajax</NavItem>
-              <NavItem href="/configuration#Class">Class</NavItem>
-              <NavItem href="/configuration#Halt">Halt</NavItem>
-              <NavItem href="/configuration#Handlers">Handlers</NavItem>
-              <NavItem href="/configuration#Listen">Listen</NavItem>
-              <NavItem href="/configuration#Message">Message</NavItem>
-              <NavItem href="/configuration#Rules">Rules</NavItem>
-              <NavItem href="/configuration#StopPropagation">StopPropagation</NavItem>
+            <ul class="navbar-nav ps-4" id="configuration">
+              <SubItem :href="{ name: 'configuration', hash: '#Ajax' }">Ajax</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Class' }">Class</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Halt' }">Halt</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Handlers' }">Handlers</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Listen' }">Listen</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Message' }">Message</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#Rules' }">Rules</SubItem>
+              <SubItem :href="{ name: 'configuration', hash: '#StopPropagation' }">
+                StopPropagation
+              </SubItem>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <NavItem href="/api">
+              <Icon icon="mdi:api" />
+              API
+            </NavItem>
+            <ul class="navbar-nav ps-4" id="api">
+              <SubItem :href="{ name: 'api', hash: '#Methods' }"> Methods </SubItem>
+              <SubItem :href="{ name: 'api', hash: '#Properties' }"> Properties </SubItem>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <NavItem href="/example">
+              <Icon icon="mdi:test-tube" />
+              Example
+            </NavItem>
+            <ul class="navbar-nav ps-4" id="example">
+              <SubItem :href="{ name: 'example', hash: '#Swal' }"> Swal </SubItem>
+              <SubItem :href="{ name: 'example', hash: '#Noty' }"> Noty </SubItem>
             </ul>
           </li>
         </ul>
@@ -72,6 +99,7 @@
 <script setup>
 import { Icon } from '@iconify/vue'
 import NavItem from './NavItem.vue'
+import SubItem from './SubItem.vue'
 </script>
 
 <style lang="scss" scoped>
