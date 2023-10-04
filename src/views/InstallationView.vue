75c99f3242d1674d80c0a38e75b05e95c5d6a019
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-10-04 21:07:03 +0800

    add installation components

--- src/views/InstallationView.vue
+++ src/views/InstallationView.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="p-5">
-    <div id="cdn">
-      <a class="link-offset-2 link-underline link-underline-opacity-0 fs-4" href="#cdn"> CDN </a>
-      <p v-for="w in 25" :key="w">...</p>
-    </div>
-    <div id="npm">
-      <a class="link-offset-2 link-underline link-underline-opacity-0 fs-4" href="#npm"> NPM </a>
-      <p v-for="w in 20" :key="w">...</p>
-    </div>
+    <CdnInstallation />
+    <NpmInstallation />
   </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue'
 import ScrollSpy from 'bootstrap/js/dist/scrollspy'
+import CdnInstallation from '../components/installation/CdnInstallation.vue'
+import NpmInstallation from '../components/installation/NpmInstallation.vue'
 
 onMounted(() => {
   new ScrollSpy(document.body, {
