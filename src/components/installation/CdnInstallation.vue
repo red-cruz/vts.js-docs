34b8fdf96ce3c101713a66cccf2286ae240c618b
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-10-04 21:06:47 +0800

    add installation components

---
+++ src/components/installation/CdnInstallation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="CDN">
+    <a class="link-offset-2 link-underline link-underline-opacity-0 fs-4" href="#CDN"> CDN </a>
+    <p v-for="w in 5" :key="w">...</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/installation/NpmInstallation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="NPM">
+    <a class="link-offset-2 link-underline link-underline-opacity-0 fs-4" href="#NPM"> NPM </a>
+    <p v-for="w in 20" :key="w">...</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
