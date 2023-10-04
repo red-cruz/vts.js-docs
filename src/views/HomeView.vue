cf42fc67cebddcbf12d446e50368c80b1a81e0e9
Raymark E. Dela Cruz <raymarkedelacruz@gmail.com>
2023-10-04 22:23:40 +0800

    add intro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <template>
-  <span v-for="i in 1000" :key="i" class="d-block">{{ i }}</span>
+  <h1 class="display-3 fw-medium">Get started with Vts.js</h1>
+  <p class="lead fs-3 lh-sm">
+    Vts is a powerful JavaScript library that simplifies form validation and asynchronous
+    submission. With Vts, you can validate form inputs and handle asynchronous requests with just a
+    <strong>single line of code</strong>.
+  </p>
 </template>
 
 <script setup></script>
